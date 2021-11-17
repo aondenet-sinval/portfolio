7222303ddd7467ced46f8c5841eb8de9bfc7e7d4
aondenet-sinval <sinvalsatv@gmail.com>
2021-11-17 20:52:15 -0300

    alguns códigos sobre manipulação do DOM e boas práticas...

---
+++ developer/12async/boaspraticas-a.js
@@ -0,0 +1,17 @@
+function testeUm() { 
+	a = 'Estou dentro';
+	b = 'Estou dentro';
+
+}
+testeUm();
+console.log('Ainda consigo ver o valor de a: ' + a);
+console.log('Ainda consigo ver o valor de b: ' + b);
+function testeDois() { 
+	var c = 'Estou dentro';
+	d = 'beleza então';
+}
+testeDois();
+//console.log('Ainda consigo ver o valor de c: ' + c);
+var d = 'Outro valor';
+console.log('d é: ',d)
+
